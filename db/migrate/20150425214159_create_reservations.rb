2c2ddb0652f5284ad41bd4120a0e9a4243d42876
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-25 17:51:59 -0400

    Creating the User, Restaurant and Reservation models with associations

---
+++ db/migrate/20150425214159_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.integer :time
+      t.integer :restaurant_id
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
