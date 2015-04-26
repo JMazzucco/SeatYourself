fc7446a5d9270fc8873e0c0798f79b26d62e1365
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-26 09:16:03 -0400

    Creating a user 'Sign up' and 'Log in' feature and a Resaurants partial

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,18 +3,46 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_url
+    else
+      render :new
+    end
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url
   end
 
-  def show
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :cuisine_type, :website)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+
+
   root 'restaurants#index'
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :restaurants do
       resources :reservations, only: [:new, :create, :destroy]
   end
