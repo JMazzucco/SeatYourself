cbd8921cb3d0b7d8dc3f24a6c366371c7c219a71
John Mazzucco <John.Mazzucco@gmail.com>
2015-08-26 09:09:26 -0400

    remove unnecessary migration to fix error in Heroku

--- db/migrate/20150702144419_remove_longitude_and_latitude_from_restaurants.rb
+++
@@ -1,6 +0,0 @@
-class RemoveLongitudeAndLatitudeFromRestaurants < ActiveRecord::Migration
-  def change
-  	remove_column(:restaurants, :Longitute)
-  	remove_column(:restaurants, :Latitude)
-  end
-end
