54bdf697cfb0241ab66b796779cf68a64b171723
John Mazzucco <John.Mazzucco@gmail.com>
2015-08-27 20:19:48 -0400

    bug fix - Time column in Reservation table was not saving

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,10 +6,7 @@ class ReservationsController < ApplicationController
   end
 
   def create
-  	@reservation = @restaurant.reservations.build(reservation_params)
-  	@reservation.user = current_user
 
-    DateTime.new()
 
     date_params = []
     5.times do |i|
@@ -17,17 +14,21 @@ class ReservationsController < ApplicationController
       date_params.push(params[:reservation]["time(" + i.to_s + "i)"].to_i)
     end
 
-    submitted_datetime = DateTime.new(date_params[0], date_params[1], date_params[2], date_params[3], date_params[4])
-    if (submitted_datetime > DateTime.now) && (submitted_datetime < 31.days.from_now)
-      @seats_booked = @restaurant.reservations.where(time: submitted_datetime).sum("party_size")
+    submitted_time = Time.new(date_params[0],date_params[1],date_params[2],date_params[3], date_params[4])
+    if (submitted_time > Time.now) && (submitted_time < 31.days.from_now)
+      @seats_booked = @restaurant.reservations.where(time: submitted_time).sum("party_size")
 
       submitted_party_size = (params[:reservation][:party_size]).to_i
+
+      @reservation = @restaurant.reservations.build(party_size: reservation_params["party_size"], time: submitted_time)
+      @reservation.user = current_user
 
       if (@seats_booked + submitted_party_size) > 100
         seats_available = (100 - @seats_booked)
         flash[:alert] = "#{seats_available} seats are available for this time"
         redirect_to restaurant_path(@restaurant)
       else
+
         if @reservation.save
           redirect_to restaurant_path(@restaurant), notice: 'Reservation is booked!'
         else
